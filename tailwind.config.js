d1b710dbaa511462304a6feffcc155b306041f27
SaiRama-Doddi <sairama1026@gmail.com>
2026-06-18 09:43:26 +0530

    style: apply Source Sans Variable (Source Sans 3) globally and soften bold text weights

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ export default {
         }
       },
       fontFamily: {
+        sans: ['"Source Sans 3"', 'sans-serif'],
         fredoka: ['"Source Sans 3"', 'sans-serif'],
         playfair: ['"Source Sans 3"', 'sans-serif'],
         outfit: ['"Source Sans 3"', 'sans-serif'],
